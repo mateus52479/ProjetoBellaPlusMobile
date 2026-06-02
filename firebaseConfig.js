5f3aef72f247eb07cf3eedec333769b9c7a94b0f
Mateus De Boit Dos Santos <mateus.248123@alunosatc.edu.br>
2026-06-02 17:22:23 -0300

    colocando o firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBad8y47P0RRvC9Sb1fgjmoe6owyclW1Ts",
+  authDomain: "bella-plus-mulherao.firebaseapp.com",
+  projectId: "bella-plus-mulherao",
+  storageBucket: "bella-plus-mulherao.firebasestorage.app",
+  messagingSenderId: "630776186556",
+  appId: "1:630776186556:web:ea8a3e4d1966d7acfcf2b9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
